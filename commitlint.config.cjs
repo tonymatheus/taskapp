8f6d1acdb68c8258fef3dfda6a424c962eb82761
tony.matheus <tony.chaves14@gmail.com>
2026-07-18 07:29:23 -0300

    chore(config): add configuration for commit process

---
+++ commitlint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'app',
+        'tasks',
+        'storage',
+        'monitoring',
+        'navigation',
+        'theme',
+        'core',
+        'deps',
+        'config',
+        'docs'
+      ]
+    ]
+  }
+};
