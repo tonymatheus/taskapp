1745457e4ee600cabbb2ed55df0b239a4b22a604
tonymatheus <tony.chaves14@gmail.com>
2026-07-23 14:13:44 -0300

    refactor(app): add load tasks and update task method

--- src/features/tasks/screens/TaskListScreen.tsx
+++ src/features/tasks/screens/TaskListScreen.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, FlatList, Text, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Button, FlatList, Text, TextInput, View } from 'react-native';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -8,9 +8,14 @@ import { TaskItem } from '../components/TaskItem';
 import { styles } from './styles';
 
 export const TaskListScreen = () => {
-  const { tasks, createTask, toggleTask, deleteTask } = useTasks();
+  const { tasks, createTask, toggleTask, deleteTask, updateTask, loadTasks } =
+    useTasks();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    loadTasks();
+  }, [loadTasks]);
 
   const handleCreateTask = async () => {
     if (!title.trim()) {
@@ -28,31 +33,40 @@ export const TaskListScreen = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Tasks</Text>
+      <View style={styles.ViewTitleContainer}>
+        <Text style={styles.title}>My Tasks</Text>
+      </View>
 
       <TextInput
-        placeholder="Title"
+        placeholder="Título"
         value={title}
         onChangeText={setTitle}
         style={styles.input}
       />
 
       <TextInput
-        placeholder="Description"
+        placeholder="Descrição (opcional)"
         value={description}
         onChangeText={setDescription}
         style={styles.input}
       />
 
-      <Button title="Add Task" onPress={handleCreateTask} />
+      <Button title="Adicionar Task" onPress={handleCreateTask} />
 
       <FlatList
         data={tasks}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
-          <TaskItem task={item} onDelete={deleteTask} onToggle={toggleTask} />
+          <TaskItem
+            task={item}
+            onDelete={deleteTask}
+            onToggle={toggleTask}
+            onEdit={updateTask}
+          />
         )}
-        ListEmptyComponent={<Text>No Content yet</Text>}
+        ListEmptyComponent={
+          <Text style={styles.empty}>Nenhuma task ainda</Text>
+        }
       />
     </SafeAreaView>
   );
